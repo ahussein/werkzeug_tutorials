66592f0ab7aa34ca9511985a882e8d12ad885cab
Abdelrahman Hussein <ahussein@codescalers.com>
2016-09-29 16:30:41 +0200

    fix import

--- shortly/shortly.py
+++ shortly/shortly.py
@@ -4,7 +4,7 @@ import urlparse
 from werkzeug.wrappers import Request, Response
 from werkzeug.routing import Map, Rule
 from werkzeug.exceptions import HTTPException, NotFound
-from werkzeug.wsgi import SharedDataMiddleWare
+from werkzeug.wsgi import SharedDataMiddleware
 from werkzeug.utils import redirect
 from jinja2 import Environment, FileSystemLoader
 
@@ -28,7 +28,7 @@ def create_app(redis_host='localhost', redis_port=6379, with_static=True):
 					'redis_port': redis_port})
 
 	if with_static:
-		app.wsgi_app = SharedDataMiddleWare(app.wsgi_app, 
+		app.wsgi_app = SharedDataMiddleware(app.wsgi_app, 
 			{
 				'/static': os.path.join(os.path.direname(__file__), 'static')
 			})
